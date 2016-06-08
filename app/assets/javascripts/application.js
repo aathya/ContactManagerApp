d05292ca7d36e9ca256dcff26eacd806e64929ad
aathy <aathy@localhost.localdomain>
2016-06-08 16:45:35 +0530

    sec kk k

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
 //= require_tree .
-//= require jquery
-//= require jquery.turbolinks
-//= require jquery_ujs
-//= require bootstrap/dropdown
 //= require turbolinks
